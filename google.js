a2e184338cdec97aa2f90736bfa4e7dc86ce7395
Daljit Singh <79385486+Daljitoo7@users.noreply.github.com>
2022-12-06 13:40:26 +0530

    Update google.js

--- google.js
+++ google.js
@@ -1,7 +1,7 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby2C-NrQht5QbMQhCyDcknbrmypZSCE38dWKC16F5K1uXBD0hrlKjWJTvwvWjRXYkTZ/exec'
             const form = document.forms['google-sheet']
           
-            form.addEventListener('submit', e => {
+            form.querySelector('submit', e => {
               e.preventDefault()
               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
                 .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
